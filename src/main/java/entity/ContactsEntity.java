cc5a612eebc66dc6683eab3efe848a409c092f36
Bryan S. Vieira <bryan.vieira2013@gmail.com>
2023-12-06 23:30:37 -0300

    Feature: create ContactsEntity

---
+++ src/main/java/entity/ContactsEntity.java
@@ -0,0 +1,29 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "contacts")
+public class ContactsEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "tell_dd")
+    private int dd;
+
+    @Column(name = "tell_number")
+    private long number;
+}
